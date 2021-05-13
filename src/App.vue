1815ede948fde8928ab1457f46c3fd35ac663dbe
Susmoy <susmoy.cse@gmail.com>
2021-05-14 02:37:57 +0600

    Add vue router transition

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,54 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + Vite" />
+	<div class="content">
+		<router-link to="/"> Home </router-link>
+		<router-link to="/guide"> Guide </router-link>
+		<router-view v-slot="{ Component }">
+			<transition name="fade" mode="out-in">
+				<component :is="Component" />
+			</transition>
+		</router-view>
+	</div>
 </template>
 
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-
 // This starter template is using Vue 3 experimental <script setup> SFCs
 // Check out https://github.com/vuejs/rfcs/blob/script-setup-2/active-rfcs/0000-script-setup.md
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+	font-family: Avenir, Helvetica, Arial, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	color: #2c3e50;
+}
+
+.content {
+	width: 40%;
+	min-width: 500px;
+	margin: 0 auto;
+	position: relative;
+}
+
+a {
+	font-weight: bold;
+	color: #2c3e50;
+	text-decoration: none;
+	margin-right: 1em;
+}
+
+a:hover,
+a.router-link-active {
+	border-bottom: 2px solid #3498db;
+}
+
+.fade-enter-from,
+.fade-leave-to {
+    opacity: 0;
+}
+
+.fade-enter-active,
+.fade-leave-active {
+    transition: opacity 0.5s ease-in;
 }
 </style>
---
+++ src/components/Guide.vue
@@ -0,0 +1,10 @@
+<template>
+    <div class="page">
+        <h1>Guide</h1>
+        <p>Lorem ipsum dolor sit amet.</p>
+        <h2>Lorem, ipsum.</h2>
+        <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore nesciunt facere soluta quia quidem et?
+        </p>
+    </div>
+</template>
